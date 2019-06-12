715c270f144b049156b25291ebfb19caf674e167
Fred Astaire <lancebc@thegodardgroup.com>
2019-06-11 21:46:44 -0400

    background image

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
 
   render() {
     return (
-      <Wrapper>;
+      <Wrapper className="background">;
         <div className="jumbotron">
           <Header>Jeu de Mémoire Stars du Cinéma Français</Header>
           <h2>Le but du jeu : marquer des points en choississant les stars une par une. Mais attention : si vous cliquez la même personne deux fois, vous perdez le jeu...</h2>
